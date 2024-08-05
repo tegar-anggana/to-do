73e38e9f19f8942b1dd1793769c51351568e70d4
Tegar Levta Anggana <tegarla123@gmail.com>
2024-08-05 10:50:59 +0700

    login signup split

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 import { useState } from "react";
-import { login, signup } from "./actions";
+import { login } from "./actions";
+import Link from "next/link";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
-  const [success, setSuccess] = useState("");
 
   const handleLogin = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
     setError(""); // Clear any previous errors
-    setSuccess(""); // Clear any previous success messages
 
     const formData = new FormData();
     formData.append("email", email);
@@ -19,63 +18,64 @@ export default function LoginPage() {
 
     const result = await login(formData);
 
-    if (result.error) {
+    if (result) {
       setError(result.error); // Set error message
     } else {
       setError("");
-      setSuccess("Login successful!"); // Set success message
-    }
-  };
-
-  const handleSignup = async (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    setError(""); // Clear any previous errors
-    setSuccess(""); // Clear any previous success messages
-
-    const formData = new FormData();
-    formData.append("email", email);
-    formData.append("password", password);
-
-    const result = await signup(formData);
-
-    if (result?.error) {
-      setError(result?.error); // Set error message
-    } else {
-      setError("");
-      setSuccess("Signup successful!"); // Set success message
     }
   };
 
   return (
     <form>
-      {error && <p className="text-red-500">{error}</p>}{" "}
-      {/* Display error message */}
-      {success && <p className="text-red-500">{success}</p>}{" "}
-      {/* Display success message */}
-      <label htmlFor="email">Email:</label>
-      <input
-        id="email"
-        name="email"
-        type="email"
-        required
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <label htmlFor="password">Password:</label>
-      <input
-        id="password"
-        name="password"
-        type="password"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" onClick={handleLogin}>
-        Log in
-      </button>
-      <button type="submit" onClick={handleSignup}>
-        Sign up
-      </button>
+      <div className="flex flex-col p-5 w-full items-center gap-4">
+        <h1 className="text-3xl">Login</h1>
+        {error && <p className="text-red-500">{error}</p>}{" "}
+        {/* Display error message */}
+        <div>
+          <div>
+            <label htmlFor="email">Email:</label>
+          </div>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-2 border-slate-600 p-2 rounded-lg"
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor="password">Password:</label>
+          </div>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 border-slate-600 p-2 rounded-lg"
+          />
+        </div>
+        <div className="mx-auto">
+          <button
+            onClick={handleLogin}
+            className="border-2 border-slate-600 px-2 py-1 rounded-lg text-lg mt-3"
+          >
+            Login
+          </button>
+        </div>
+        <div>
+          <p>
+            Don't have an account?{" "}
+            <Link href={"/signup"} className="underline">
+              Register
+            </Link>
+          </p>
+        </div>
+      </div>
     </form>
   );
 }
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,81 @@
-import React from 'react'
+"use client";
+import { useState } from "react";
+import { signup } from "./actions";
+import Link from "next/link";
 
-export default function page() {
+export default function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignup = async (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    setError(""); // Clear any previous errors
+
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+
+    const result = await signup(formData);
+
+    if (result) {
+      setError(result.error); // Set error message
+    } else {
+      setError("");
+    }
+  };
+
   return (
-    <div>page</div>
-  )
+    <form>
+      <div className="flex flex-col p-5 w-full items-center gap-4">
+        <h1 className="text-3xl">Register</h1>
+        {error && <p className="text-red-500">{error}</p>}{" "}
+        {/* Display error message */}
+        <div>
+          <div>
+            <label htmlFor="email">Email:</label>
+          </div>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="border-2 border-slate-600 p-2 rounded-lg"
+          />
+        </div>
+        <div>
+          <div>
+            <label htmlFor="password">Password:</label>
+          </div>
+          <input
+            id="password"
+            name="password"
+            type="password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="border-2 border-slate-600 p-2 rounded-lg"
+          />
+        </div>
+        <div className="mx-auto">
+          <button
+            onClick={handleSignup}
+            className="border-2 border-slate-600 px-2 py-1 rounded-lg text-lg mt-3"
+          >
+            Register
+          </button>
+        </div>
+        <div>
+          <p>
+            Already have an account?{" "}
+            <Link href={"/login"} className="underline">
+              Login
+            </Link>
+          </p>
+        </div>
+      </div>
+    </form>
+  );
 }
